0876b78251133a24b010d3276d485df70f6b57e7
Kyle Bader <kyle.bader@gmail.com>
2013-01-22 17:16:08 +0000

    add cron cookbook derived from opscode-cookbooks/cron

---
+++ cookbooks/cron/metadata.rb
@@ -0,0 +1,9 @@
+name             "cron"
+maintainer       "Kyle Bader"
+maintainer_email "kyle.bader@gmail.com"
+license          "Apache 2.0"
+description      "Configures vixie-cron"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.3.0"
+
+supports gentoo
